afb9fcee6a5f3314c947e60c37a68dde75385439
Emiel Stoelinga <emielstoelinga@gmail.com>
2017-05-14 19:58:35 +0200

    Include Emiels running game 2

---
+++ Assets/Scripts/PlatformGenerator.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformGenerator : MonoBehaviour {
+
+	public GameObject thePlatform;
+	public Transform generationPoint;
+	public float distanceBetween;
+
+	private float platformWidth;
+
+	public float distanceBetweenMin;
+	public float distanceBetweenMax;
+
+	//public GameObject[] thePlatforms;
+	private int platformSelector;
+	private float[] platformWidths;
+
+	public ObjectPooler[] theObjectPools;
+
+	// Use this for initialization
+	void Start () {
+		//platformWidth = thePlatform.GetComponent<BoxCollider2D> ().size.x;
+
+		platformWidths = new float[theObjectPools.Length];
+
+		for (int i = 0; i < theObjectPools.Length; i++) {
+			platformWidths [i] = theObjectPools[i].pooledObject.GetComponent<BoxCollider2D> ().size.x;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (transform.position.x < generationPoint.position.x) {
+
+			distanceBetween = Random.Range (distanceBetweenMin, distanceBetweenMax);
+
+			transform.position = new Vector3 (transform.position.x + (platformWidths[platformSelector] / 2) + distanceBetween, transform.position.y, transform.position.z);
+
+			platformSelector = Random.Range (0, theObjectPools.Length);
+
+			//Instantiate (thePlatforms[platformSelector], transform.position, transform.rotation);
+
+			GameObject newPlatform = theObjectPools[platformSelector].GetPooledObject();
+
+			newPlatform.transform.position = transform.position;
+			newPlatform.transform.rotation = transform.rotation;
+			newPlatform.SetActive (true);
+
+
+			transform.position = new Vector3 (transform.position.x + (platformWidths[platformSelector] / 2), transform.position.y, transform.position.z);
+
+		}
+
+	}
+}
---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+
+public class PlayerControl : MonoBehaviour {
+
+	public float moveSpeed;
+	public float jumpForce;
+	public float walkJumpForce;
+
+	public float jumpTime;
+	private float jumpTimeCounter;
+
+	private Rigidbody2D myRigidbody;
+
+	private bool grounded;
+	public LayerMask whatIsGround;
+	public Transform groundCheck;
+	public float groundCheckRadius;
+
+	public GameManager theGameManager;
+
+	private Collider2D myCollider;
+
+	public ScoreManager myScoreManager;
+
+	// Use this for initialization
+	void Start () {
+		myRigidbody = GetComponent<Rigidbody2D> ();
+
+		myCollider = GetComponent<Collider2D> ();
+
+		jumpTimeCounter = jumpTime;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		// grounded = Physics2D.IsTouchingLayers (myCollider, whatIsGround);
+
+		grounded = Physics2D.OverlapCircle (groundCheck.position, groundCheckRadius, whatIsGround);
+
+		if (grounded && Physics2D.IsTouchingLayers(myCollider, whatIsGround)) {
+			myRigidbody.velocity = new Vector2 (myRigidbody.velocity.x, walkJumpForce);
+		}
+
+		int fingerCount = 0;
+		foreach (Touch touch in Input.touches) {
+			if (touch.phase != TouchPhase.Ended && touch.phase != TouchPhase.Canceled)
+				fingerCount++;
+		}
+
+		if (fingerCount == 1) {
+			if (jumpTimeCounter > 0) {
+				myRigidbody.velocity = new Vector2 (myRigidbody.velocity.x, jumpForce);
+				jumpTimeCounter -= Time.deltaTime;
+			}
+		}
+
+		if (fingerCount == 0) {
+			jumpTimeCounter = 0;
+		}
+
+		if (grounded) {
+			jumpTimeCounter = jumpTime;
+		}
+
+		myRigidbody.velocity = new Vector2 (moveSpeed, myRigidbody.velocity.y);	
+	}
+
+	void OnCollisionEnter2D(Collision2D other) {
+		if (other.gameObject.tag == "killbox") {
+			GameObject.Find ("Game").GetComponent<game_script> ().Back (myScoreManager.scoreCount);
+		}
+	}
+}
