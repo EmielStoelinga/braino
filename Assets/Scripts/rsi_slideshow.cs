8b0e3b4f75849133b94b1cb65edbd6d45215c4f0
Emiel Stoelinga <emielstoelinga@gmail.com>
2017-05-16 11:09:06 +0200

    Add correct RSI images and reward

--- Assets/Scripts/rsi_slideshow.cs
+++ Assets/Scripts/rsi_slideshow.cs
@@ -9,19 +9,27 @@ public class rsi_slideshow : MonoBehaviour {
 	public List<Sprite> images = new List<Sprite>();
 	public float showspeed;
 	public int imagecount;
+
+	public int reward;
+
 	private List<Sprite> slideshow = new List<Sprite>();
 	private int index = 0;
 	private float timer = 0f;
 
 	// Use this for initialization
 	void Start () {
-		Screen.orientation = ScreenOrientation.Portrait;
+		//Screen.orientation = ScreenOrientation.Portrait;
+		for (int i = 0; i < imagecount; i++) {
+			slideshow.Add (images[i]);
+		}
+		/*
 		while (slideshow.Count < imagecount) {
 			int randomindex = Random.Range(0, images.Count);
 			if (!slideshow.Contains (images [randomindex])) {
 				slideshow.Add (images[randomindex]);
 			}
 		}
+		*/
 	}
 	
 	// Update is called once per frame
@@ -33,6 +41,12 @@ public class rsi_slideshow : MonoBehaviour {
 			index++;
 		}
 
+		if (timer >= showspeed && index >= slideshow.Count-1) {
+			GameObject.Find ("Game").GetComponent<game_script> ().Back (reward);
+		}
+
 		rsiImage.sprite = slideshow [index];
+
+
 	}
 }
