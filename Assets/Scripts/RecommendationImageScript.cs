2b116d753e5b99b5b82ebee8d9706befeb6dd398
Emiel Stoelinga <emielstoelinga@gmail.com>
2017-06-06 16:04:42 +0200

    Include recommendation balloon functionality

--- Assets/Scripts/RecommendationImageScript.cs
+++ Assets/Scripts/RecommendationImageScript.cs
@@ -6,18 +6,25 @@ using UnityEngine.UI;
 
 public class RecommendationImageScript : MonoBehaviour {
 
+	public float percentageWidth;
+	public float percentageHeight;
+
+	public float width;
+	public float height;
+
 	private RawImage image;
 
 	// Use this for initialization
 	void Start () {
 		image = gameObject.GetComponent<RawImage> ();
+
+		width = (int)(percentageWidth/100 * Screen.width);
+		height = (int)(percentageHeight/100 * Screen.height);
+
+		image.GetComponent<RectTransform> ().sizeDelta = new Vector2(width, height);
 	}
 
 	// Update is called once per frame
 	void Update () {
-		var width = (int)(.25 * Screen.width);
-		var height = (int)(.2 * Screen.height);
-
-		image.GetComponent<RectTransform> ().sizeDelta = new Vector2(width, height);
 	}
 }
---
+++ Assets/Scripts/RecommendationTextScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class RecommendationTextScript : MonoBehaviour {
+	
+	private Text text;
+
+	// Use this for initialization
+	void Start () {
+		text = gameObject.GetComponent<Text> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		text.text = "this is a recommendation this is a recommendation this is a recommendation";
+
+		// insert here when to change the recommendation
+
+	}
+}
